1bdb1f15511b5fe12e9a07ce137208a9925a75a6
Songyuan Hai <Songyuan@Songyuans-MBP.domain>
2017-12-17 14:51:52 -0700

    add post project master maintenance

--- WebContent/vicaraapp/ProjectMasterRegistration.controller.js
+++ WebContent/vicaraapp/ProjectMasterRegistration.controller.js
@@ -1,38 +1,134 @@
-sap.ui.controller("vicaraapp.ProjectMasterRegistration", {
+sap.ui.controller("vicaraapp.ProjectMasterRegistration",
+		{
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf vicaraapp.ProjectMasterRegistration
-*/
-//	onInit: function() {
-//
-//	},
+			/**
+			 * Called when a controller is instantiated and its View controls
+			 * (if available) are already created. Can be used to modify the
+			 * View before it is displayed, to bind event handlers and do other
+			 * one-time initialization.
+			 * 
+			 * @memberOf vicaraapp.ProjectMasterRegistration
+			 */
+			// onInit: function() {
+			//
+			// },
+			projectRegistration : function(oEvt) {
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf vicaraapp.ProjectMasterRegistration
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+				var oPspElement = sap.ui.getCore().byId("idPspElementInput")
+						.getValue();
+				var oProjectName = sap.ui.getCore().byId("idProjectNameInput")
+						.getValue();
+				var oStartDate = sap.ui.getCore().byId(
+						"idProjectStartDatePicker").getValue();
+				var oEndDate = sap.ui.getCore().byId("idProjectEndDatePicker")
+						.getValue();
+				var oFixedPrice = sap.ui.getCore().byId("idFixedPriceInput")
+						.getSelectedItem().getText();
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf vicaraapp.ProjectMasterRegistration
-*/
-//	onAfterRendering: function() {
-//
-//	},
+				var priceFlag = true;
+				if (oFixedPrice === "False") {
+					priceFlag = false;
+				}
+				console
+						.log(oPspElement + "-----" + oProjectName + "-----"
+								+ oStartDate + "-----" + oEndDate + "-----"
+								+ priceFlag);
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf vicaraapp.ProjectMasterRegistration
-*/
-//	onExit: function() {
-//
-//	}
+				if (oPspElement === "" || oProjectName === ""
+						|| oStartDate === "" || oEndDate === ""
+						|| oFixedPrice === "" || oPspElement === undefined
+						|| oProjectName === undefined
+						|| oStartDate === undefined || oEndDate === undefined
+						|| oFixedPrice === undefined) {
+					sap.m.MessageToast
+							.show("Please enter all required fields!");
+				} else {
 
-});
+					var submitData = {
+						psp_element : oPspElement,
+						project_name : oProjectName,
+						start_date : oStartDate,
+						end_date : oEndDate,
+						fixed_price : priceFlag,
+					}
+
+					$.ajax({
+						method : 'POST',
+						url : 'http://127.0.0.1:8000/api/v1/project_master/',
+						data : submitData,
+						// dataType : 'json',
+						// crossDomain : true,
+						success : function(data, textStatus, jqXHR) {
+							sap.m.MessageToast.show("Create success!");
+//							var oFirstName = sap.ui.getCore().byId(
+//									"idFirstNameInput");
+//							var oMiddleName = sap.ui.getCore().byId(
+//									"idMiddleNameInput");
+//							var oLastName = sap.ui.getCore().byId(
+//									"idLastNameInput");
+//							var oEmployeeID = sap.ui.getCore().byId(
+//									"idEmployeeIDInput");
+//							var oEmail1 = sap.ui.getCore()
+//									.byId("idEmail1Input");
+//							var oEmail2 = sap.ui.getCore()
+//									.byId("idEmail2Input");
+//							var oPassword1 = sap.ui.getCore().byId(
+//									"idPassword1Input");
+//							var oPassword2 = sap.ui.getCore().byId(
+//									"idPassword2Input");
+//							oFirstName.setValue();
+//							oMiddleName.setValue();
+//							oLastName.setValue();
+//							oEmployeeID.setValue();
+//							oEmail1.setValue();
+//							oEmail2.setValue();
+//							oPassword1.setValue();
+//							oEmail2.setValue();
+							app.back();
+							console.log(data);
+							console.log(textStatus);
+							console.log(jqXHR);
+						},
+						error : function(res, xhr, readyState) {
+							sap.m.MessageToast
+									.show("Create new user failed! Code: "
+											+ res.responseText);
+							console.log(res.responseText);
+							console.log(xhr);
+							console.log("ERROR: " + readyState);
+						}
+					});
+				}
+
+			},
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the
+		 * controller's View is re-rendered (NOT before the first rendering!
+		 * onInit() is used for that one!).
+		 * 
+		 * @memberOf vicaraapp.ProjectMasterRegistration
+		 */
+		// onBeforeRendering: function() {
+		//
+		// },
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the
+		 * document). Post-rendering manipulations of the HTML could be done
+		 * here. This hook is the same one that SAPUI5 controls get after being
+		 * rendered.
+		 * 
+		 * @memberOf vicaraapp.ProjectMasterRegistration
+		 */
+		// onAfterRendering: function() {
+		//
+		// },
+		/**
+		 * Called when the Controller is destroyed. Use this one to free
+		 * resources and finalize activities.
+		 * 
+		 * @memberOf vicaraapp.ProjectMasterRegistration
+		 */
+		// onExit: function() {
+		//
+		// }
+		});
--- WebContent/vicaraapp/ProjectMasterRegistration.view.js
+++ WebContent/vicaraapp/ProjectMasterRegistration.view.js
@@ -18,7 +18,100 @@ sap.ui.jsview("vicaraapp.ProjectMasterRegistration", {
 	 * 
 	 * @memberOf vicaraapp.ProjectMasterRegistration
 	 */
-	createContent : function(oController) {// Add footer and copyright
+	createContent : function(oController) {
+
+		// Input PSP element
+		var oPspElementLabel = new sap.m.Label("idPspElementLabel", {
+			text : "PSP Element",
+			required : true,
+		}).addStyleClass("userRegistrationInputsCss");
+
+		var oPspElementInput = new sap.m.Input("idPspElementInput", {
+			placeholder : "Please enter PSP element",
+			valueStateText : "PSP element cannot be empty.",
+			liveChange : function() {
+				if (this.getValue() === "") {
+					// if the field is empty after change, it will go red
+					this.setValueState(sap.ui.core.ValueState.Error);
+				} else {
+					// if the field is not empty after change, the value state
+					// (if any) is removed
+					this.setValueState(sap.ui.core.ValueState.None);
+				}
+			}
+		}).addStyleClass("userRegistrationInputsCss");
+
+		// Input project name
+		var oProjectNameLabel = new sap.m.Label("idProjectNameLabel", {
+			text : "Project Name",
+			required : true,
+		}).addStyleClass("userRegistrationInputsCss");
+
+		var oProjectNameInput = new sap.m.Input("idProjectNameInput", {
+			placeholder : "Please enter project name",
+			valueStateText : "Project name cannot be empty.",
+			liveChange : function() {
+				if (this.getValue() === "") {
+					// if the field is empty after change, it will go red
+					this.setValueState(sap.ui.core.ValueState.Error);
+				} else {
+					// if the field is not empty after change, the value state
+					// (if any) is removed
+					this.setValueState(sap.ui.core.ValueState.None);
+				}
+			}
+		}).addStyleClass("userRegistrationInputsCss");
+
+		// input project start date
+		var oProjectStartDateLabel = new sap.m.Label("idProjectStartDateLabel",
+				{
+					text : "Start Date",
+					required : true,
+				}).addStyleClass("userRegistrationInputsCss");
+
+		var oProjectStartDatePicker = new sap.m.DatePicker(
+				"idProjectStartDatePicker", {
+					valueFormat : "yyyy-MM-dd",
+				}).addStyleClass("userRegistrationInputsCss");
+
+		// input project end date
+		var oProjectEndDateLabel = new sap.m.Label("idProjectEndDateLabel", {
+			text : "End Date",
+			required : true,
+		}).addStyleClass("userRegistrationInputsCss");
+
+		var oProjectEndDatePicker = new sap.m.DatePicker(
+				"idProjectEndDatePicker", {
+					valueFormat : "yyyy-MM-dd",
+				}).addStyleClass("userRegistrationInputsCss");
+
+		// input fixed price
+		var oProjectFixedPriceLabel = new sap.m.Label("idProjectFixedPriceLabel", {
+			text : "Fixed Price",
+			required : true,
+		}).addStyleClass("userRegistrationInputsCss");
+		
+		// Define items in fixed price
+		var oFixedTrue = new sap.ui.core.Item({
+			text : "True",
+		});
+
+		var oFixedFalse = new sap.ui.core.Item({
+			text : "False",
+		});
+		
+		// Define Select fixed price List
+		var oFixedPriceInput = new sap.m.Select("idFixedPriceInput", {
+			items : [ oFixedTrue, oFixedFalse,],
+		}).addStyleClass("userRegistrationInputsCss");
+		
+		// Create project button
+		var oCreateProjectButton = new sap.m.Button("idCreateProjectButton", {
+			text : "Create Project",
+			press : [ oController.projectRegistration, oController ]
+		}).addStyleClass("btnCss");
+		
+		// Add footer and copyright
 		var oFooter = new sap.m.Bar({
 			design : sap.m.BarDesign.Footer,
 			contentMiddle : [ new sap.m.Label({
@@ -33,12 +126,24 @@ sap.ui.jsview("vicaraapp.ProjectMasterRegistration", {
 			width : "120px",
 		});
 
+//		var oFlexBox = new sap.m.FlexBox({
+//			direction : "Column",
+//			alignItems : "Center",
+//			justifyContent : "Center",
+//			fitContainer : true,
+//			items : [],
+//		});
+		
 		var oPage = new sap.m.Page({
 			title : "Project Master Registration",
 			headerContent : oLogoImage,
 			footer : oFooter,
-			content : [],
+			content : [oPspElementLabel, oPspElementInput, oProjectNameLabel,
+				oProjectNameInput, oProjectStartDateLabel,
+				oProjectStartDatePicker, oProjectEndDateLabel,
+				oProjectEndDatePicker,oProjectFixedPriceLabel,oFixedPriceInput,oCreateProjectButton],
 			showNavButton : true,
+			enableScrolling : false,
 			navButtonPress : function(oEvt) {
 				app.back();
 			},
